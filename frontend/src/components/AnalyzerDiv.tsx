10f430a6c7a26b0679362bedc87053b421155a1e
Shivaansh-Sharma <shivaanshsharma.business@gmail.com>
2025-10-14 09:53:54 +0530

    UI Improvement

--- frontend/src/components/AnalyzerDiv.tsx
+++ frontend/src/components/AnalyzerDiv.tsx
@@ -1,26 +1,31 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 import { motion } from "motion/react";
 import TypingEffect1 from "@/components/HomeDiv3Typing1";
 
-
 const AnalyzerDiv1 = () => {
   return (
-    <motion.div  initial={{ scale: 0 }} animate={{ scale: 1 }} className='py-10 flex justify-between items-center'>
-        <div className="text-center ">
-          <h1 className="text-5xl text-center font-bold mb-4">
-            Text Analyzer
-          </h1>
-          <p className="text-xl text-left text-[var(--color-Text)]">
-           Our advanced text analyzer helps you go beyond simple writing checks by offering powerful tools to refine your content. It instantly measures word count, detects language, and identifies plagiarism while also analyzing tone and sentiment. With grammar check included, it ensures clarity, accuracy, and impact, making your writing more professional, engaging, and reliable for any purpose.
-          </p>
-          <br />
-         <TypingEffect1 />
+    <motion.div
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      className="min-h-screen flex justify-center items-center py-10"
+    >
+      <div className="text-center max-w-3xl">
+        <h1 className="text-5xl font-bold mb-4">Text Analyzer</h1>
+        <p className="text-xl text-center text-[var(--color-Text)]">
+          Our advanced text analyzer helps you go beyond simple writing checks
+          by offering powerful tools to refine your content. It instantly
+          measures word count, detects language, and identifies plagiarism while
+          also analyzing tone and sentiment. With grammar check included, it
+          ensures clarity, accuracy, and impact, making your writing more
+          professional, engaging, and reliable for any purpose.
+        </p>
+        <br />
+        <TypingEffect1 />
+      </div>
+    </motion.div>
+  );
+};
 
-        </div>
-        </motion.div>
-  )
-}
-
-export default AnalyzerDiv1
+export default AnalyzerDiv1;
